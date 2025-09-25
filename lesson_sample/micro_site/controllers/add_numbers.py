ea22d848f6521f577dff863525492429f8a82344
aaavau <kmm2459306@stu.o-hara.ac.jp>
2025-09-25 11:56:57 +0900

    get_additionを作成

--- lesson_sample/micro_site/controllers/add_numbers.py
+++ lesson_sample/micro_site/controllers/add_numbers.py
@@ -1,11 +1,12 @@
 from utils import render_template
-from app_logic import set_first_value, set_second_value
+from app_logic import set_first_value, set_second_value, get_addition
 
 
 def add_numbers(environ):
     #ここに処理を書く
     set_first_value(10)
     set_second_value(20)
+    addition = get_addition()
     # set_first_value()
     # get_addition()
 
